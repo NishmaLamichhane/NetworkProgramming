24ff522a6dc5627797d2656f3c3d4469fa3c6e4e
NishmaLamichhane <nishmalamichhane2005@gmail.com>
2026-01-03 21:34:50 +0545

    Server side SocketChanner

---
+++ SocketChannelServer.java
@@ -0,0 +1,25 @@
+//Server Side Socket Channel Example
+import java.io.*;
+import java.net.*;
+import java.nio.*;
+import java.nio.channels.*;
+import java.nio.file.*;
+
+public class SocketChannelServer {
+   public static void main(String[] args)throws IOException {
+    SocketChannel Server=SocketChannel.open();
+    SocketAddress address=new InetSocketAddress("localhost",9000);
+    Server.connect(address);
+    Path path=Paths.get("C:/Test/temp.txt");
+    FileChannel fileChannel=FileChannel.open(path);
+    ByteBuffer buffer=ByteBuffer.allocate(1024);
+    while (fileChannel.read(buffer)>0) {
+        buffer.flip();
+        Server.write(buffer);
+        buffer.clear();
+    }
+    fileChannel.close();
+    System.out.println("File sent successfully.");
+    Server.close();
+   } 
+}
